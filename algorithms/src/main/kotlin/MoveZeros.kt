788be7328a4c3cec481677029162668651753e94
VladSobo <thecodegod1@gmail.com>
2025-02-08 22:40:25 +0500

    MoveZeros.kt solution. O(n) time O(n) space

---
+++ algorithms/src/main/kotlin/MoveZeros.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val array = readln().split(" ").map { it.toInt() }.toIntArray()
+    Solution().moveZeroes(array)
+}
+
+class Solution {
+    fun moveZeroes(nums: IntArray){
+        var writeIndex = 0
+        for (readIndex in nums.indices) {
+            if (nums[readIndex] != 0) {
+                val buffer = nums[writeIndex]
+                nums[writeIndex] = nums[readIndex]
+                nums[readIndex] = buffer
+                writeIndex++
+            }
+        }
+    }
+}
